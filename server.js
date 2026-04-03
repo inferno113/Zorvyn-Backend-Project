34f59f7da5e8f908d509673961314d8d7377ad55
inferno113 <tanishkdeore04@gmail.com>
2026-04-03 15:22:07 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./backend/config/db");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "API is running" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
